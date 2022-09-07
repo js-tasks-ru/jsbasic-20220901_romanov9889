6076d33fac8b3df5b8677d7be9dd76b67d22295e
Dmitriy Romanov <romanov9889@gmail.com>
2022-09-07 03:50:30 +0300

    Последние задачи

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
+const SPAM1 = '1xbet';
+const SPAM2 = 'xxx'
 function checkSpam(str) {
-  // ваш код...
+  if (str.toLowerCase().indexOf(SPAM1) >= 0 || str.toLowerCase().indexOf(SPAM2) >= 0){
+    return true
+} else {
+    return false;
+}
 }
