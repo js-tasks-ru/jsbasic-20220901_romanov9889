1d38eba86498ab134eb8700758bfc60ca0dfc3ed
Dmitriy Romanov <romanov9889@gmail.com>
2022-10-17 21:49:14 +0500

    3 задача 4 модуля

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,22 @@
+const ageIndex = 1;
+const genderIndex = 2;
+const statusIndex = 3;
+
+const isAvailable = (cell) => cell.getAttribute('data-available') === "true" ? 'available' : 'unavailable';
+
+const maleOrFemale = (cell) => cell.innerText === `m` ? 'male' : 'female';
+
+const ageCheck = (row,cell) => {if (cell.innerText < 18) {row.style="text-decoration: line-through";}}
+
 function highlight(table) {
-  // ваш код...
+  
+  for (let i = 1; i < table.rows.length; i += 1){
+    const row = table.rows[i];
+    
+    row.cells[statusIndex].hasAttribute('data-available') ? row.classList.add(isAvailable(row.cells[statusIndex])) : row.hidden = true;
+    
+    row.classList.add(maleOrFemale(row.cells[genderIndex]));
+   
+    ageCheck(row, row.cells[ageIndex]);
+  }
 }
