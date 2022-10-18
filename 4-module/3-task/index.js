4f0143549a74785a0d9aa3d6f31b99dc15238282
Dmitriy Romanov <romanov9889@gmail.com>
2022-10-18 11:50:18 +0500

    поправил 3 задачу

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,22 +1,32 @@
+const isAvailable = (row) => {
+
+  if (row.cells[statusIndex].hasAttribute('data-available')) {
+
+    row.cells[statusIndex].getAttribute('data-available') === "true" ? row.classList.add('available') : row.classList.add('unavailable');
+    
+  } else {
+
+    row.hidden = true;
+
+  }
+}
+
+const maleOrFemale = (row) => row.cells[genderIndex].innerText === `m` ? row.classList.add('male') : row.classList.add('female');
+
+const ageCheck = (row) => {if (row.cells[ageIndex].innerText < 18) {row.style="text-decoration: line-through";}}
+
 const ageIndex = 1;
 const genderIndex = 2;
 const statusIndex = 3;
-
-const isAvailable = (cell) => cell.getAttribute('data-available') === "true" ? 'available' : 'unavailable';
-
-const maleOrFemale = (cell) => cell.innerText === `m` ? 'male' : 'female';
-
-const ageCheck = (row,cell) => {if (cell.innerText < 18) {row.style="text-decoration: line-through";}}
 
 function highlight(table) {
   
   for (let i = 1; i < table.rows.length; i += 1){
     const row = table.rows[i];
     
-    row.cells[statusIndex].hasAttribute('data-available') ? row.classList.add(isAvailable(row.cells[statusIndex])) : row.hidden = true;
+    isAvailable(row);
+    maleOrFemale(row);
+    ageCheck(row);
     
-    row.classList.add(maleOrFemale(row.cells[genderIndex]));
-   
-    ageCheck(row, row.cells[ageIndex]);
   }
 }
